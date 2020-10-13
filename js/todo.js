eb3caf082752e12b345bc1610f1944d15d4a09b3
Young Rong Oh <youngrongoh@Youngui-MacBookPro.local>
2020-10-13 20:29:11 +0900

    JS To do

---
+++ js/todo.js
@@ -0,0 +1,64 @@
+const toDoForm = document.querySelector('.js-toDoForm');
+const toDoInput = toDoForm.querySelector('input');
+const toDoList = document.querySelector('.js-toDoList');
+
+const TODOS_LS = 'toDos';
+
+let toDos = [];
+
+function deleteToDo(event) {
+  const btn = event.target;
+  const li = btn.parentNode;
+  toDoList.removeChild(li);
+  const cleanToDos = toDos.filter(toDo => {
+    return toDo.id !== parseInt(li.dataset.id)
+  });
+  toDos = cleanToDos;
+  saveToDos();
+}
+
+function saveToDos() {
+  localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
+function paintToDo(text) {
+  const li = document.createElement('li');
+  const delBtn = document.createElement('button');
+  const span = document.createElement('span');
+  const newId = toDos.length + 1;
+  delBtn.textContent = '❌';
+  delBtn.addEventListener('click', deleteToDo);
+  span.textContent = text;
+  li.appendChild(delBtn);
+  li.appendChild(span);
+  li.setAttribute('data-id', newId);
+  toDoList.appendChild(li);
+  const toDoObj = {
+    text,
+    id: newId,
+  }
+  toDos.push(toDoObj);
+  saveToDos();
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const currentValue = toDoInput.value;
+  paintToDo(currentValue);
+  toDoInput.value = '';
+}
+
+function loadToDos() {
+  const loadedToDos = localStorage.getItem(TODOS_LS);
+  if(loadedToDos !== null) {
+    const parsedToDos = JSON.parse(loadedToDos);
+    parsedToDos.forEach(toDo => paintToDo(toDo.text));
+  }
+}
+
+function init() {
+  loadToDos();
+  toDoForm.addEventListener('submit', handleSubmit);
+}
+
+init();
